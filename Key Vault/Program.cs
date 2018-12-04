09b3285fc3637dbab6334981ab48169ed616f617
er.kkmohanty@gmail.com <er.kkmohanty@gmail.com>
2018-12-04 12:38:25 +0530

    key vault

--- Key Vault/Program.cs
+++ Key Vault/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using System;
 
 namespace Key_Vault
 {
@@ -6,7 +8,12 @@ namespace Key_Vault
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            var secret = keyVaultClient.GetSecretAsync("https://keyvaultkk.vault.azure.net/secrets/TheSecret")
+                .Result;
+            Console.WriteLine("Secret is::" + secret.Value);
+            Console.ReadLine();
         }
     }
 }
